c3eee891961ef96a4278b4d2a6947e84972ae860
Burns <burns@anderswe.lt>
2020-01-30 12:26:15 +0100

    Changed CSS names from "admin/ma" to "purple/green"

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Anderswelt Planungskosmos</title>
-    <link rel="stylesheet" href="<?php echo BASE_URL . "css/style_admin.css"; ?>" media="screen" />
+    <link rel="stylesheet" href="<?php echo BASE_URL . "css/style_purple.css"; ?>" media="screen" />
     <link rel="stylesheet" href="<?php echo BASE_URL . "css/style_base.css"; ?>" media="screen" />
     <script src="https://kit.fontawesome.com/cb1ea7256d.js"></script>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
